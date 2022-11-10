7c538d1136c65d1ae091ff7e1351a9d1e3797c20
Leticia <leticia.marinho532@gmail.com>
2022-11-09 21:44:20 -0300

    feat: use axios + create service to use axios + create register sale page

--- front-end/src/routes/router.js
+++ front-end/src/routes/router.js
@@ -1,8 +1,10 @@
 import Home from '@/pages/Home.vue';
+import RegisterSale from '@/pages/RegisterSale.vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/cadastrar-venda', component: RegisterSale },
 ];
 
 const router = createRouter({
