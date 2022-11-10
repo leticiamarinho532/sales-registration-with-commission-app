e93abf0f35a0746f7074c4f9b495e61fac5afafa
Leticia <leticia.marinho532@gmail.com>
2022-11-09 23:00:07 -0300

    feat: remove not in use codes + create list all sellers page

---
+++ front-end/src/pages/ListSellers.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h3>Lista de Vendedores</h3>
+        <ul v-for="(seller,index) in sellers" :key="index">
+            <li>
+                <b>-</b>
+                Id: {{seller.id}}
+                Nome: {{seller.name}}
+                email: {{seller.email}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import api from '@/services/api.js';
+
+export default {
+    name: 'ListSellers',
+    data() {
+        return {
+            sellers: []
+        }
+    },
+    methods: {
+        async getSellers() {
+            console.log('aqui');
+            await api.get('/seller/show/').then(response => {
+                this.sellers = response.data.message;
+            });
+        }
+    },
+    mounted() {
+        this.getSellers();
+    }
+}
+</script>
+<style scoped>
+    div {
+        width: 100%;
+        text-align: center;
+        position: relative;
+        top: 50%;
+        transform: translateY(50%);
+    }
+</style>
---
+++ front-end/src/pages/RegisterSeller.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <form @submit="createSeller">
+            <label for="name">Nome:</label><br>
+            <input type="text" id="name" name="name" v-model="name" /><br>
+            <label for="email">E-mail:</label><br>
+            <input type="text" id="email" name="email" v-model="email"/><br><br>
+            <input type="submit" value="Submit">
+        </form> 
+    </div>
+</template>
+
+<script>
+import api from '@/services/api.js';
+
+export default {
+    name: 'RegisterSeller',
+    data() {
+        return {
+            name: null,
+            email: null
+        }
+    },
+    methods: {
+        async createSeller(e) {
+            e.preventDefault();
+
+            const data = {
+                name: this.name,
+                email: this.email
+            };
+
+            await api.post('/seller/create/', data).then(response => {
+                alert(response.data.message);
+            })
+            
+        }
+    }
+}
+</script>
+<style scoped>
+    div {
+        width: 100%;
+        
+    }
+
+    div {
+        text-align: center;
+        position: relative;
+        top: 50%;
+        transform: translateY(50%);
+    }
+</style>
